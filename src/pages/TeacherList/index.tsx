46131c0f066230a5b8cf7b9a056513132e0c57c8
Benício Barreto <benicio.daniel@gmail.com>
2020-08-04 22:46:59 -0300

    Added style to teacher-item mobile

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
 import "./styles.css";
 
 function TeacherList() {
@@ -24,6 +26,34 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars3.githubusercontent.com/u/52285940?s=460&u=62458ff1b9ec02aac67bb56fed2eaadb0ad16b33&v=4"
+              alt="Beni"
+            />
+            <div>
+              <strong>Benício Daniel</strong>
+              <span>Architecture</span>
+            </div>
+          </header>
+
+          <p>Teste teste teste</p>
+
+          <footer>
+            <p>
+              Preço / hora
+              <strong>R$ 80.00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="WhatsApp Icon" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
