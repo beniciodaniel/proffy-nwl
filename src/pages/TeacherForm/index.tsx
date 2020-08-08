f575de63705db84335bde79d0a22b61a2510acbf
Benício Barreto <benicio.daniel@gmail.com>
2020-08-08 17:13:07 -0300

    Implemented history push to landing page after saving classes

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Textarea from '../../components/Textarea';
@@ -9,6 +10,8 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 import api from '../../services/api';
 
 function TeacherForm() {
+  const history = useHistory();
+
   const [name, setName] = useState('');
   const [avatar, setAvatar] = useState('');
   const [whatsapp, setWhatsapp] = useState('');
@@ -65,6 +68,7 @@ function TeacherForm() {
       })
       .then(() => {
         alert('DEU CERTO BEIBE');
+        history.push('/');
       })
       .catch(() => {
         alert('DEU RUIM');
